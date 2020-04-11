9a90ee252f071120daa86870504d3fe6b6210bd1
asaadco <asaadge@gmail.com>
2020-04-11 23:12:27 +0300

    initialization

---
+++ beeplayer/src/main/java/com/beecastproject/beeplayer/beeplayer.java
@@ -0,0 +1,13 @@
+package com.beecastproject.beeplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class beeplayer extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_beeplayer);
+  }
+}
